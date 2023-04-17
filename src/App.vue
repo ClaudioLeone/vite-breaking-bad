54e5e7c86741ec08d11561cb98589826dac76d76
ClaudioLeone <claleone92@gmail.com>
2023-04-17 17:51:37 +0200

    Created and added 'store.js' file for state management

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script>
-
+  import { store } from "./store";
 
   export default {
-
+    data() {
+      return {
+        store
+      }
+    }
   }
 </script>
 
 <template>
-
+ <h1 v-for="(greeting, index) in store.greetings" :key="index">{{ greeting }}</h1>
 </template>
 
 <style scoped lang="scss">
